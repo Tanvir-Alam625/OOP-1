abd9e90897c3cbc77ecbdbc7e97b109f2fab899d
tanveer <mdtanviralamht731@gmail.com>
2022-02-15 00:47:33 +0600

    splice file added

---
+++ splice.js
@@ -0,0 +1,6 @@
+const fewNumbers = [13,21,351,5,5,551,35,54,5,65,35,454,21,4,64,4,54,54,5,2,42,1];
+// const cutNumber = fewNumbers.splice(4,5);
+// console.log(fewNumbers.length);
+const cutNumber = fewNumbers.splice(22,0,56,56,45,643,46346345,3463423,)
+console.log(cutNumber);
+console.log(fewNumbers);
