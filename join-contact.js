6f40e9e417d290841a53bc01826001d34636fbeb
tanveer <mdtanviralamht731@gmail.com>
2022-02-14 23:25:45 +0600

    join-contact file added

---
+++ join-contact.js
@@ -0,0 +1,9 @@
+const likeOneNameArray = ['tanvir', 'alam', 'shakila', 'monju', 'jorina', 'shine'];
+// akta array ar poti ta element akti particular charecter string create kora jai  join method dia
+// const howIsWorking = likeOneNameArray.join(' ');
+// const howIsWorking = likeOneNameArray.join(', ');
+// console.log(howIsWorking);
+const firstString = 'Lorem Ipsum';
+const secondString = 'my names';
+const add = firstString.concat(secondString);
+console.log(add);
