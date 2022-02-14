70014f2ab67bf3d359ead16a16008d37b5e51aba
tanveer <mdtanviralamht731@gmail.com>
2022-02-14 23:18:12 +0600

    slice,substr,substring file added

---
+++ slice,substr,substring.js
@@ -0,0 +1,8 @@
+const mySummary = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry  standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book';
+// first value is start the index number and second value end index number
+// const custom = mySummary.slice(5, 20);
+// first value start the index of number and second value how many index in show
+// const custom = mySummary.substr(5, 10);
+// first value is start the index number and second value end index number
+const custom = mySummary.substring(5, 10);
+console.log(custom);
